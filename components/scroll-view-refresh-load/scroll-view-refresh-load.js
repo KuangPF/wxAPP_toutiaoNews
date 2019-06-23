4e8b77be872474fbe45ac3ff2cfd86712aad8fe7
KuangPF <1633397286@qq.com>
2019-06-23 15:58:09 +0800

    chore: init demo

--- components/scroll-view-refresh-load/scroll-view-refresh-load.js
+++ components/scroll-view-refresh-load/scroll-view-refresh-load.js
@@ -4,20 +4,29 @@ Component({
    * 组件的属性列表
    */
   properties: {
-
+    pullDownText: {
+      type: String,
+      value: '下拉刷新'
+    }
   },
 
   /**
    * 组件的初始数据
    */
   data: {
-
+    pullDownStatus: 0 // 下拉刷新的状态
   },
 
   /**
    * 组件的方法列表
    */
   methods: {
-
+    onScroll: e => {
+      // console.log(e)
+    },
+    /* 下拉刷新 */
+    pullDownRefresh: e => {
+      console.log(e)
+    }
   }
 })
