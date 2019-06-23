b4d257556269fc534eb228c5a179188a30387729
KuangPF <1633397286@qq.com>
2019-06-23 16:29:17 +0800

    mod: handle onScroll

--- components/scroll-view-refresh-load/scroll-view-refresh-load.js
+++ components/scroll-view-refresh-load/scroll-view-refresh-load.js
@@ -7,6 +7,20 @@ Component({
     pullDownText: {
       type: String,
       value: '下拉刷新'
+    },
+    /* 是否需要下拉 */
+    pullDown: {
+      type: Boolean,
+      value: true
+    },
+    /* 下拉触发刷新的高度 */
+    pullDownHeight: {
+      type: Number,
+      value: 60
+    },
+    releaseText: {
+      type: String,
+      value: '松开刷新'
     }
   },
 
@@ -21,12 +35,26 @@ Component({
    * 组件的方法列表
    */
   methods: {
-    onScroll: e => {
-      // console.log(e)
+    onScroll: function(e) {
+      this.triggerEvent('scroll', e.detail)
+      const status = this.data.pullDownStatus
+      const height = this.properties.pullDownHeight
+      const scrollTop = e.detail.scrollTop
+      let targetStatus
+      if (scrollTop < -1 * height) {
+        targetStatus = 2
+      } else if (scrollTop < 0) {
+        targetStatus = 1
+      } else {
+        targetStatus = 0
+      }
+      if (status != targetStatus) {
+        this.setData({
+          pullDownStatus: targetStatus
+        })
+      }
     },
     /* 下拉刷新 */
-    pullDownRefresh: e => {
-      console.log(e)
-    }
+    pullDownRefresh: function(e) {}
   }
 })
